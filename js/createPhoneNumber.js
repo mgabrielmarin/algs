2695f8215dfd7cdbaf4e4500aea1ca4214166d4e
gbrlmarn <gbrlmarn@gmail.com>
2022-01-10 12:14:17 +0200

    simple phone number creator function

---
+++ js/createPhoneNumber.js
@@ -0,0 +1,6 @@
+function createPhoneNumber(numbers) {
+  if(numbers.length == 10)
+    return "(" + number.slice(0, 3).join('') + ") " + 
+      number(3, 6).join('') + "-" + numbers(6, 10).join('');
+  return null;
+}
